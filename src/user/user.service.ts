952d879a7b3effa2f8efb01726c09d9d21b3f60d
Smerlyn-Javier <smerlyn88@gmail.com>
2021-02-21 15:18:50 -0400

    [ADD] model to user service 📥

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { userSchemaDocument, User } from 'src/schema/user.schema';
+
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(@InjectModel(User.name) private UserModel: Model<userSchemaDocument>) { }
+
+    async findAllUsers() {
+        return await this.UserModel.find();
+    }
+    async findOneUser(userId) {
+        return await this.UserModel.findById(userId);
+    }
+    async createOneUser(userInfo) {
+        return await this.UserModel.create(userInfo);
+    }
+    async updateOneUser(userId,userInfo) {
+        return await this.UserModel.findByIdAndUpdate(userId,userInfo);
+    }
+    async deleteOneUser(userId) {
+        return await this.UserModel.findByIdAndDelete(userId);
+    }
+
+}
