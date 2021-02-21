9868132217059b256fc4e9e31b8d281210ebb66d
Smerlyn-Javier <smerlyn88@gmail.com>
2021-02-21 12:15:15 -0400

    [ADD] mongoose module connection to app module 🔌

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://smerlyn88:kQ5EO7qemmB8VtM6@cluster0.z3wbt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
