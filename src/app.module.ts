98c700e1e3474542850edee7e4dd931f3437e1ff
Smerlyn-Javier <smerlyn88@gmail.com>
2021-02-21 18:24:40 -0400

    [ADD] role module 🔌

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,18 @@ import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { RoleController } from './role/role.controller';
+import { RoleService } from './role/role.service';
+import { RoleModule } from './role/role.module';
 
 
 @Module({
   imports: [
-    UserModule,
     MongooseModule.forRoot('mongodb+srv://smerlyn88:kQ5EO7qemmB8VtM6@cluster0.z3wbt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
+    UserModule,
+    RoleModule,
   ],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  controllers: [AppController, UserController, RoleController],
+  providers: [AppService, UserService, RoleService],
 })
 export class AppModule { }
