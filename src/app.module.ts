d53625248f8465a4a176740cb307e6f5420e3027
Smerlyn-Javier <smerlyn88@gmail.com>
2021-02-21 15:12:09 -0400

    [ADD] user module and mongo uri connection  🔌

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,15 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://smerlyn88:kQ5EO7qemmB8VtM6@cluster0.z3wbt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot('mongodb+srv://smerlyn88:kQ5EO7qemmB8VtM6@cluster0.z3wbt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
+  ],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
-export class AppModule {}
+export class AppModule { }
