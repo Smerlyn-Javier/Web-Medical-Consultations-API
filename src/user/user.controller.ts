b3ae222e9e20e0f15b27cfeb50f6cdedf23c37cb
Smerlyn-Javier <smerlyn88@gmail.com>
2021-02-21 15:16:48 -0400

    [ADD] service to controller 🚛

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAllUsers() {
+        return this.userService.findAllUsers();
+    }
+    @Get(':userId')
+    @HttpCode(HttpStatus.OK)
+    findOneUsers(@Param('userId') userId: string) {
+        return this.userService.findOneUser(userId);
+    }
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    createOneUser(@Body() createUserDto: CreateUserDto) {
+        return this.userService.createOneUser(createUserDto);
+    }
+    @Patch(':userId')
+    @HttpCode(HttpStatus.ACCEPTED)
+    updateOneUser(@Param('userId') userId, @Body() userInfo) {
+        return this.userService.updateOneUser(userId,userInfo);
+    }
+    @Delete(':userId')
+    @HttpCode(HttpStatus.OK)
+    deleteOneUser(@Param('userId') userId: string) {
+        return this.userService.deleteOneUser(userId);
+    }
+}
