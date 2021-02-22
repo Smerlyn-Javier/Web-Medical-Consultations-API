12940644155df3b3ccdb77b89d6ea96af2a2c164
Smerlyn Javier <smerlyn88@gmail.com>
2021-02-22 16:43:39 -0400

    [ADD] symptom controller 📄

---
+++ src/schema/symptom.schema.ts
@@ -0,0 +1,18 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose'
+import { Document} from 'mongoose'
+import * as mongoose from 'mongoose';
+
+export type symptomSchemaDocument = Symptom & Document;
+
+@Schema()
+export class Symptom {
+    @Prop()
+    symptomName: string;
+    @Prop()
+    symptomDescription: string;
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Status' })
+    statusId: string;
+}
+
+export const SymptomSchema = SchemaFactory.createForClass(Symptom);
